f95450b19f357e6bfc535cc682055e1d65745333
sorayams <somasa.laquais@gmail.com>
2022-06-11 16:22:06 +0700

    Poster Image, Year, Plot
    update connect firebase for poster, year, plot.

--- MenuListMovie/app/src/main/java/com/example/menulistmovie/MoviesAdapter.kt
+++ MenuListMovie/app/src/main/java/com/example/menulistmovie/MoviesAdapter.kt
@@ -2,26 +2,65 @@ package com.example.menulistmovie
 
 import android.app.AlertDialog
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
+import java.util.concurrent.Executors
 
 class MoviesAdapter(val mContext : Context, val layoutResId : Int, val movList: List<Movies>) : ArrayAdapter<Movies>(mContext,layoutResId,movList){
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layoutInflater : LayoutInflater = LayoutInflater.from(mContext)
         val view : View = layoutInflater.inflate(layoutResId, null)
         val tvNama : TextView = view.findViewById(R.id.tvnama)
-        val tvEdit : TextView = view.findViewById(R.id.tvedit)
-
+        val tvNamaKet : TextView = view.findViewById(R.id.tvnamaKet)
+        val tahun : TextView = view.findViewById(R.id.tvnamayear)
+        val poster : ImageView = view.findViewById(R.id.poster)
         val movies : Movies = movList[position]
-        tvEdit.setOnClickListener {
-            showSearchDialog(movies)
+
+        var image: Bitmap? = null
+        val executor = Executors.newSingleThreadExecutor()
+        // Once the executor parses the URL
+        // and receives the image, handler will load it
+        // in the ImageView
+        val handler = Handler(Looper.getMainLooper())
+
+        executor.execute {
+
+            // Tries to get the image and post it in the ImageView
+            // with the help of Handler
+            try {
+                val `in` = java.net.URL(movies.poster).openStream()
+                image = BitmapFactory.decodeStream(`in`)
+
+                // Only for making changes in UI
+                handler.post {
+                    poster.setImageBitmap(image)
+                }
+            }
+
+            // If the URL does not point to
+            // image or any other kind of failure
+            catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+
         tvNama.text = movies.title
+        tvNamaKet.text = movies.plot
+        tahun.text = movies.year
+
+
+
         return view
+
     }
 
     private fun showSearchDialog(movies: Movies) {
