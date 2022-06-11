b7e53d9179593515fff3a7ffe256569a5f86d2f7
adam <adam.ichsan@code.id>
2022-06-11 11:26:22 +0700

    add login register

---
+++ MenuListMovie/app/src/main/java/com/example/menulistmovie/LoginActivity.kt
@@ -0,0 +1,77 @@
+package com.example.menulistmovie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var btnRegister : Button
+    private lateinit var btnLogin : Button
+    private lateinit var txtEmail : EditText
+    private lateinit var txtPassword : EditText
+    private lateinit var otentikasi : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        otentikasi = FirebaseAuth.getInstance()
+        btnRegister = findViewById(R.id.bRegister)
+        txtEmail = findViewById(R.id.etEmail)
+        txtPassword = findViewById(R.id.etPassword)
+        btnLogin = findViewById(R.id.bLogin)
+
+        btnLogin.setOnClickListener {
+            val email = txtEmail.text.toString().trim()
+            val password = txtPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                txtEmail.error = "Email tidak boleh kosong"
+                txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty() || password.length < 6){
+                txtPassword.error = "Password tidak boleh kosong dan minimal 6 karakter"
+                txtPassword.requestFocus()
+                return@setOnClickListener
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                txtEmail.error = "Format penulisan Email Salah"
+                txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            loginUser(email, password)
+        }
+
+        btnRegister.setOnClickListener {
+            Intent(this@LoginActivity, RegisterActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+    private fun loginUser(email: String, password: String) {
+        otentikasi.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Intent(this@LoginActivity, SearchActivity::class.java).also { intent ->
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+                    }
+                }else{
+                    Toast.makeText(this,"${it.exception?.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    override fun onStart() {
+        super.onStart()
+        if(otentikasi.currentUser != null) {
+            Intent(this@LoginActivity, SearchActivity::class.java).also { intent ->
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ MenuListMovie/app/src/main/java/com/example/menulistmovie/RegisterActivity.kt
@@ -0,0 +1,76 @@
+package com.example.menulistmovie
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var btnLogin : Button
+    private lateinit var btnSimpan : Button
+    private lateinit var txtEmail : EditText
+    private lateinit var txtPassword : EditText
+    private lateinit var otentikasi : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        otentikasi = FirebaseAuth.getInstance()
+        btnSimpan = findViewById(R.id.bSimpan)
+        btnLogin = findViewById(R.id.bLogin)
+        txtEmail = findViewById(R.id.etEmailReg)
+        txtPassword = findViewById(R.id.etPasswordReg)
+
+        btnSimpan.setOnClickListener {
+            val email = txtEmail.text.toString().trim()
+            val password = txtPassword.text.toString().trim()
+
+            if (email.isEmpty()){
+                txtEmail.error = "Email tidak boleh kosong"
+                txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            if (password.isEmpty() || password.length < 6){
+                txtPassword.error = "Password tidak boleh kosong dan minimal 6 karakter"
+                txtPassword.requestFocus()
+                return@setOnClickListener
+            }
+            if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+                txtEmail.error = "Format penulisan Email Salah"
+                txtEmail.requestFocus()
+                return@setOnClickListener
+            }
+            registerUser(email, password)
+        }
+        btnLogin.setOnClickListener {
+            Intent(this@RegisterActivity, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+    private fun registerUser(email: String, password: String) {
+        otentikasi.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if(it.isSuccessful){
+                    Intent(this@RegisterActivity, SearchActivity::class.java).also {
+                        it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(it)
+                    }
+                }else{
+                    Toast.makeText(this, it.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+    override fun onStart() {
+        super.onStart()
+        if(otentikasi.currentUser != null) {
+            Intent(this@RegisterActivity, SearchActivity::class.java).also { intent ->
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+            }
+        }
+    }
+}
